b96d4d8e8286e6e82835e86fb74a9ad0a1d92f84
royer <arthur.royer.nogueira@escola.pr.gov.br>
2026-05-25 11:14:38 -0300

    ..

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Hello, World!");
 int idade = 19;  //Guarda números inteiros
 string nome = "lins";    //Guarda textos
-bool programador = true; //Guarda verdadeiro ou falso
+bool programador = "true"; //Guarda verdadeiro ou falso
 if (idade >= 18)
 {
     Console.WriteLine("você e maior de idade.");
